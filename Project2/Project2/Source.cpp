12d15778a8c17d9a4dbd30f5351ce06483af3a3c
Globalika <volodymyr.seredovych@gmail.com>
2020-05-31 15:37:59 +0200

    last change

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -2,34 +2,39 @@
 
 using namespace std;
 
-int main()
+bool isPrime(int n)
 {
-	int i, n;
-	cout << "enter simple number\n";
-	cin >> n;
-	
-	if(n==1)
-	{
-		cout << "number is not simple\n";
+	if (n == 1) {
+		return false;
 	}
-	else if(n == 2)
-		cout << "number simple\n";
-	else if (n == 3)
-		cout << "number simple\n";
+	else if (n == 2 || n == 3) {
+		return true;
+	}
+	else {
 
-	
-	for(i=2;i<=(int)n/2;i++)
-	{
-		if (n % i == 0) 
+		for (int i = 2; i <= (int)n / 2; i++)
 		{
-			cout << "number is not simple\n";
-			break;
-		}
+			if (n % i == 0)
+			{
+				return false;
+			}
 
-		if (i == (int)n / 2)
-		{
-			cout << "number simple\n";
-			break;
+			if (i == (int)n / 2)
+			{
+				return true;
+			}
 		}
 	}
 }
+
+int main()
+{
+	int  n;
+	cout << "enter number\n";
+	cin >> n;
+	cout << isPrime(n);
+
+}
+
+
+
