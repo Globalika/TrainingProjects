2a36ee447a0428cba91882f6a8b4036afe1ba4e7
Globalika <volodymyr.seredovych@gmail.com>
2020-05-31 14:23:03 +0200

    add new proj

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i, n;
+	cout << "enter simple number\n";
+	cin >> n;
+	
+	if(n==1)
+	{
+		cout << "number is not simple\n";
+	}
+	else if(n == 2)
+		cout << "number simple\n";
+	else if (n == 3)
+		cout << "number simple\n";
+
+	
+	for(i=2;i<=(int)n/2;i++)
+	{
+		if (n % i == 0) 
+		{
+			cout << "number is not simple\n";
+			break;
+		}
+
+		if (i == (int)n / 2)
+		{
+			cout << "number simple\n";
+			break;
+		}
+	}
+}
